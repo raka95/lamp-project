1d872e56c48ce200103a5a671c2ad078efbdb1d2
raminkarimian <ramin-ka@raminkarimians-MacBook-Pro.local>
2018-01-28 11:19:57 +0330

    second commit

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,6 +5,7 @@ import {history} from "../routers/AppRouter";
 export const LoginPage=()=>{
     return (
         <div className="box-layout"> 
+        
             <form onSubmit={()=>history.push('/register')} className=" box-layout__2">
                 <h1 className="box-layout__title">LogIn Page</h1>
                 <input className="text-input" placeholder="Enter your Email Address" required={true}/>
